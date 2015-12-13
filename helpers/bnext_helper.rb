e9bd78f85d1f748f133766457fedcf5817362382
Jacky Pan <jackypan000@gmail.com>
2015-12-13 14:04:37 +0800

    Remove helpers folders

--- helpers/bnext_helper.rb
+++
@@ -1,9 +0,0 @@
-##
-# Helpers for main Sinatra web application
-module BNextHelpers
-  def get_ranks(ranktype, category, page)
-    RankFeeds.fetch(ranktype, category, page)
-  rescue
-    halt 404
-  end
-end
