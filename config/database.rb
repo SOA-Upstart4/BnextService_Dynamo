27d43822da14707cb2850649859fe074967c49f4
Jacky Pan <jackypan000@gmail.com>
2015-12-21 11:18:40 +0800

    Revise Dynamo configuration

--- config/database.rb
+++ config/database.rb
@@ -4,6 +4,6 @@ Dynamoid.configure do |config|
   config.adapter = 'aws_sdk_v2'
   config.namespace = 'bnext_api'
   config.warn_on_scan = false
-  config.read_capacity = 10
-  config.write_capacity = 10
+  config.read_capacity = 3
+  config.write_capacity = 3
 end
