ab3c600d07170eb70b8cee89d85b55c6005e5d10
Jacky Pan <jackypan000@gmail.com>
2015-12-24 22:59:33 +0800

    Make the association between article & tag table for better keyword query performance

--- models/article.rb
+++ models/article.rb
@@ -6,7 +6,9 @@ class Article
   field :title, :string
   field :author, :string
   field :date, :string
-  field :tags, :set
+  #field :tags, :set
+  #has_many :tags
+  has_and_belongs_to_many :tags
   field :link, :string
 
   def self.destroy(id)
@@ -16,4 +18,23 @@ class Article
   def self.delete_all
     all.each(&:delete)
   end
+end
+
+
+class Tag
+  include Dynamoid::Document
+  table :name => :tags, :read_capacity => 3, :write_capacity => 3
+  field :word, :string
+
+  has_and_belongs_to_many :articles
+
+
+  def self.destroy(id)
+    find(id).destroy
+  end
+
+  def self.delete_all
+    all.each(&:delete)
+  end
+
 end
